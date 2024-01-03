fa5b305f285aa8548fc1547efb5ce5744ec4c994
sirjadejaa <jadejanildeepsinh80030@gmail.com>
2024-01-03 09:07:21 +0530

    03/01/2024⠀⎦⎡⠀09:07⠀⎦⎢⠀Wednesday

--- sample-project/resources/views/welcome.blade.php
+++ sample-project/resources/views/welcome.blade.php
@@ -6,26 +6,40 @@
     <style>
     </style>
     <div class="row home-container m-lg-5">
+
+        {{$userName}}
+
         <div class="col-md-8 justify-content-center align-items-center d-flex">
             <div>
-            <h1 class="heading text-center">Hello World.</h1>
-            <p class="home-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, ab minima esse alias quo accusantium architecto eius unde odit, eos consequuntur enim inventore odio voluptates doloribus explicabo ipsam. Fuga, architecto?
-            </p>
-            </div>  
+                <h1 class="heading text-center">Hello World.</h1>
+                <p class="home-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, ab minima esse alias quo
+                    accusantium architecto eius unde odit, eos consequuntur enim inventore odio voluptates doloribus
+                    explicabo ipsam. Fuga, architecto?
+                </p>
+            </div>
         </div>
 
         <div class="col-md-4">
-                <img class="img-home" src="{{asset('/images/pic2.png')}}" alt="kites">
+            <img class="img-home" src="{{asset('/images/pic2.png')}}" alt="kites">
         </div>
     </div>
 
-        <div class="row justify-content-center">
-            <div class="col-md-3 shadow-sm  post-container">
-                <h3 class="heading">Kites</h3>
-                <img src="{{asset('/images/pic2.png')}}" alt="sdfs" class="post-img">
-                <span class="post-text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quas impedit odio culpa at excepturi consequatur et necessitatibus dolorem aut quos obcaecati unde reiciendis vero, voluptatibus distinctio quae rem nulla!</span>
-            </div>
+    <div class="row justify-content-center">
+
+
+        @foreach($list as $id=>$node)
+        <div class="col-md-3 m-3 shadow-sm  post-container">
+            <h3 class="heading">{{$id}}</h3>
+            <span>{{$node['name']}}</span>
+            {{$node['img']}}<img src="{{ asset('/images/' . $node['img']) }}" alt="sdfs" class="post-img">
+
+            <span class="post-text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quas impedit odio
+                culpa at excepturi consequatur et necessitatibus dolorem aut quos obcaecati unde reiciendis vero,
+                voluptatibus distinctio quae rem nulla!</span>
         </div>
+        @endforeach
+
+    </div>
 </div>
 @endsection
--- sample-project/routes/web.php
+++ sample-project/routes/web.php
@@ -14,7 +14,17 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    // fetch code from db
+    $name="gk sir";
+
+    $data = array(
+        "KIENFU793H"=>array("name"=>"awseK","place"=>"hell","img"=>"pic2.png"),
+        "DKJFI3593H"=>array("name"=>"KSI","place"=>"heaven","img"=>"pic2.png"),
+        "DHD45HGFH3"=>array("name"=>"asfk","place"=>"hell","img"=>"pic2.png"),
+        "DFGKJDFO4K"=>array("name"=>"hfh","place"=>"same as u","img"=>"pic2.png"),
+    );
+    return view('welcome',['userName'=>$name,'list'=>$data]);
 });
 
 
